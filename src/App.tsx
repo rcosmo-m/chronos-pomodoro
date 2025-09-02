3d1cb3f23e9e1bf056b14034adf3168fc4be5563
Raphael Menezes <raphaelcm.br@hotmail.com>
2025-09-02 19:39:00 -0300

    feat: set up HTML and CSS structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="content">
+          <section>Logo</section>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="container">
+        <div className="content">
+          <section>Menu</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>Form</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>Footer</section>
+        </div>
+      </div>
+    </>
+  );
+}
