7d95173eba57949120f4ae8e8979addf093e1530
Raphael Cosmo Menezes <raphaelcm.br@hotmail.com>
2026-03-23 15:48:02 -0300

    feat: handle task creation with useRef and form submit

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,27 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form className='form'>
+    <form
+      onSubmit={handleCreateNewTask}
+      className='form'
+    >
       <div className='formRow'>
         <DefaultInput
           labeltext='Tarefa'
           id='myinput'
           type='text'
           placeholder='Digite sua Tarefa'
+          ref={taskNameInput}
         />
       </div>
 
