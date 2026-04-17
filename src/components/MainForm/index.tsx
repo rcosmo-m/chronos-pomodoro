09651e89a5b3a720e1739ad6ccbb9fa3a3d3c70e
Raphael Cosmo Menezes <raphaelcm.br@hotmail.com>
2026-04-17 15:06:17 -0300

    feat: add next cycle type logic to task creation

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,12 +6,14 @@ import { useRef } from 'react';
 import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaxtContext';
 import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -31,8 +33,8 @@ export function MainForm() {
       startDate: Date.now(),
       completedDate: null,
       interruptedDate: null,
-      duration: 1,
-      type: 'workTime',
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
